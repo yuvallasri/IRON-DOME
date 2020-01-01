4cfaf4d3a1b4f1474675cfea1eec2976b52e739e
yuvallasri <49484083+yuvallasri@users.noreply.github.com>
2020-01-01 22:57:28 +0200

    first game commit - yuval

---
+++ Assets/myRocket.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class myRocket : MonoBehaviour
+{
+    public AudioClip impact;
+    AudioSource bom;
+   
+    public GameObject rocket;
+    GameObject rocketclone;
+    private void Start()
+    {
+
+        bom = GetComponent<AudioSource>();
+
+    }
+    void Update()
+    {
+
+
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+
+        {
+
+            rocketclone = Instantiate(rocket, transform.position, Quaternion.identity) as GameObject;
+            bom.PlayOneShot(impact, 1.0f);
+            Destroy(rocketclone, 4);
+
+        }
+    }
+
+}
---
+++ Assets/rocketForce.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocketForce : MonoBehaviour
+{
+   
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = this.transform.position;
+        position.y++;
+        position.z--;
+        position.z--;
+        position.z--;
+        position.z--;
+                this.transform.position = position;
+    }
+}
