295b8136aed0ac6b4343d2a8ebb42555ec4c46c2
yuvallasri <yuvallasri04@gmail.com>
2020-01-09 10:52:07 +0200

    level1 update

--- Assets/myRocket.cs
+++ Assets/myRocket.cs
@@ -5,19 +5,12 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class myRocket : MonoBehaviour
 {
-    public AudioClip impact;
-    AudioSource bom;
+   
    
     public GameObject rocket;
     GameObject rocketclone;
-    private void Start()
-    {
-
-        bom = GetComponent<AudioSource>();
-
-    }
-    void Update()
-    {
+       void Update()
+    { 
 
 
         if (Input.GetKeyUp(KeyCode.Mouse0))
@@ -25,14 +18,9 @@ public class myRocket : MonoBehaviour
         {
 
             rocketclone = Instantiate(rocket, transform.position, Quaternion.identity) as GameObject;
-            bom.PlayOneShot(impact, 1.0f);
-            Destroy(rocketclone, 4);
+            Destroy(rocketclone, 10);
 
         }
     }
-    private void OnDestroy()
-    {
-        
-    }
-
+ 
 }
