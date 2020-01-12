6d7ae620160c9cb17f91ee27dfe1cf970ca1076b
yuvallasri <yuvallasri04@gmail.com>
2020-01-12 15:41:36 +0200

    fix anemy missile + cross

---
+++ Assets/anemymissilemovment.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class anemymissilemovment : MonoBehaviour
+{
+    public GameObject anemymissile;
+    private Transform rockettarget;
+    public Rigidbody missilerig;
+    public float turn;
+    public float missilevel;
+    public GameObject explosioneffect;
+
+    void Update()
+    {
+         
+        missilerig.velocity = transform.forward * missilevel;
+        rockettarget = GameObject.FindWithTag("home").transform;
+        var missiletargetrotation = Quaternion.LookRotation(rockettarget.position - transform.position);
+        missilerig.MoveRotation(Quaternion.RotateTowards(transform.rotation, missiletargetrotation, turn));
+
+    }
+    void DestroyGameObject()
+    {
+        var mewposition = new Vector3(Random.Range(-150, 150), 200, 12);
+
+        Instantiate(anemymissile, mewposition, Quaternion.identity);
+        Instantiate(explosioneffect, transform.position, transform.rotation);
+
+        Destroy(gameObject);
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+               DestroyGameObject();
+    }
+
+    void OnDestroy()
+    {
+        
+
+    }
+}
