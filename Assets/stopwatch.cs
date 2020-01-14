fe1914166c802e0bb96b6bf46367720bb600bc4f
yuvallasri <yuvallasri04@gmail.com>
2020-01-14 10:29:32 +0200

    add timer

---
+++ Assets/stopwatch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class stopwatch : MonoBehaviour
+{
+    public float timeLeft = 40.0f;
+    public Text startText; 
+
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        startText.text = (timeLeft).ToString("0");
+        
+    }
+}
