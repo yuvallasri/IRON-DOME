13be3b03493f4506c1af4d03fb31faacb4fb277e
yuvallasri <yuvallasri04@gmail.com>
2020-01-14 14:29:06 +0200

    nightvision

---
+++ Assets/car_target/carmovment2.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carmovment2 : MonoBehaviour
+{
+    public GameObject anemymissile;
+    private Transform rockettarget;
+    public Rigidbody missilerig;
+    public float turn;
+    public float missilevel;
+    public GameObject explosioneffect;
+
+    void Update()
+    {
+         
+        missilerig.velocity = transform.forward * missilevel;
+        rockettarget = GameObject.FindWithTag("home").transform;
+        var missiletargetrotation = Quaternion.LookRotation(rockettarget.position - transform.position);
+        missilerig.MoveRotation(Quaternion.RotateTowards(transform.rotation, missiletargetrotation, turn));
+
+    }
+    void DestroyGameObject()
+    {
+        var mewposition = new Vector3(-265, 8, 255);
+
+        Instantiate(anemymissile, mewposition, this.transform.rotation);
+        Instantiate(explosioneffect, transform.position, transform.rotation);
+
+        Destroy(gameObject);
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+               DestroyGameObject();
+    }
+
+    void OnDestroy()
+    {
+        
+
+    }
+}
---
+++ Assets/show_cursor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class show_cursor : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+
+    }
+
+}
