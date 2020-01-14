3f316302d70f02231dabfbff28e4f038ff0b400e
yuvallasri <yuvallasri04@gmail.com>
2020-01-14 11:41:07 +0200

    exit add

---
+++ Assets/exit.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class exit : MonoBehaviour
+{
+    public void scenloader(int scennumber)
+    {
+        if(scennumber==1)
+
+        Application.Quit();
+    }
+        
+    
+}
