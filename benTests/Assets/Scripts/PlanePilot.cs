0a7c1e1674b6dda94c88d547f92b104fb93bb5a2
Benjamin Ahr <benahr36@gmail.com>
2020-01-18 18:37:03 +0200

    Fixed another level - more enemies, faster missiles

---
+++ benTests/Assets/Scripts/PlanePilot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanePilot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("script for" + gameObject.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
